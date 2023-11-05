1e80d8579e57f6c94837fda44eea219c8f128d4e
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2023-11-05 16:45:36 -0500

    collision detection

--- loca cocina/Assets/Code/Player/Player.cs
+++ loca cocina/Assets/Code/Player/Player.cs
@@ -5,16 +5,80 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float movedSpeedField = 7f;
-    [SerializeField] Animator animatorCP;
+
     bool isWalking;
     void Update()
     {
-        Vector2 inputVector = GetComponent<GameInput>().GetMovementVector();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        isWalking = moveDir != Vector3.zero;
-        transform.position += moveDir * Time.deltaTime * movedSpeedField;
+        HandheldMove();
+    }
+
+    private void HandheldMove()
+    {
+        Vector2 _inputVector = GetComponent<GameInput>().GetMovementVector();
+        Vector3 _moveDir = new Vector3(_inputVector.x, 0f, _inputVector.y);
+
+        float _moveDistance = movedSpeedField * Time.deltaTime;
+        float _playerRadius = .7f;
+        float _playerHeight = 2f;
+        bool _canMove = !Physics.CapsuleCast(
+                                                transform.position,
+                                                transform.position + Vector3.up * _playerHeight,
+                                                _playerRadius,
+                                                _moveDir,
+                                                _moveDistance
+                                            );
+        if (!_canMove)
+        {
+            //can not move towards moveDir
+
+            //attempt only x movement
+            Vector3 _moveDirX = new Vector3(_moveDir.x, 0, 0).normalized;
+            _canMove = !Physics.CapsuleCast(
+                                                transform.position,
+                                                transform.position + Vector3.up * _playerHeight,
+                                                _playerRadius,
+                                                _moveDirX,
+                                                _moveDistance
+                                            );
+            if (_canMove)
+            {
+                //Can move only on the X
+                _moveDir = _moveDirX;
+            }
+            else
+            {
+                // cannot move ony on the X
+
+
+                // Attempt only Z movement
+                Vector3 _moveDirZ = new Vector3(0, 0, _moveDir.z).normalized;
+                _canMove = !Physics.CapsuleCast(
+                                                    transform.position,
+                                                    transform.position + Vector3.up * _playerHeight,
+                                                    _playerRadius,
+                                                    _moveDirZ,
+                                                    _moveDistance
+                                                );
+
+                if (_canMove)
+                {
+                    //Can move only on the Z
+                    _moveDir = _moveDirZ;
+                }
+                else
+                {
+                    //can't move in any direction
+                }
+            }
+
+        }
+        if (_canMove)
+        {
+            transform.position += _moveDir * Time.deltaTime * movedSpeedField;
+        }
+        isWalking = _moveDir != Vector3.zero;
         float _rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * _rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, _moveDir, Time.deltaTime * _rotateSpeed);
     }
 
     public bool GetIsWalking()
