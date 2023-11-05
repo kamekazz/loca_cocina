20fbef2a30aa8838964938b8d051432b30ff813a
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2023-11-05 01:23:24 -0500

    input System Refactor

--- loca cocina/Assets/Code/Controls/GameInput.cs
+++ loca cocina/Assets/Code/Controls/GameInput.cs
@@ -4,28 +4,16 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    PlayerActionsControls playerInputActions;
+    void Awake()
+    {
+        playerInputActions = new PlayerActionsControls();
+        playerInputActions.Player.Enable();
+    }
     public Vector2 GetMovementVector()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-
-        }
         inputVector = inputVector.normalized;
         return inputVector;
     }
