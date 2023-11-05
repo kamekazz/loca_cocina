18286654f65abbcd0e63fafd1074fdd509476951
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2023-11-05 01:07:53 -0500

    clean code

--- loca cocina/Assets/Code/Player/Player.cs
+++ loca cocina/Assets/Code/Player/Player.cs
@@ -9,27 +9,7 @@ public class Player : MonoBehaviour
     bool isWalking;
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = GetComponent<GameInput>().GetMovementVector();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * Time.deltaTime * movedSpeedField;
