a709537451b05bb224b74560da5e57a1a30c151d
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2023-11-06 15:46:20 -0500

    kitchen object parent

--- loca cocina/Assets/Code/Iteam/ClearCounter.cs
+++ loca cocina/Assets/Code/Iteam/ClearCounter.cs
@@ -6,12 +6,59 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] KitchenObjectSO kitchenObjectSO;
     [SerializeField] Transform counterTopPointTF;
+    [SerializeField] ClearCounter secondClearCounter;
+    [SerializeField] bool testing;
+    KitchenObject kitchenObjectSP;
+
+    void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+
+            if (kitchenObjectSP != null)
+            {
+                kitchenObjectSP.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
+
+
+
     public void Interact()
     {
-        Debug.Log("Interact");
-        Transform kitchenObjectTF = Instantiate(kitchenObjectSO.gameObject.transform, counterTopPointTF);
-        kitchenObjectTF.localPosition = Vector3.zero;
+        if (kitchenObjectSP == null)
+        {
+            Transform kitchenObjectTF = Instantiate(kitchenObjectSO.gameObject.transform, counterTopPointTF);
+            kitchenObjectTF.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObjectSP.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTF.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPointTF;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObjectSP)
+    {
+        this.kitchenObjectSP = kitchenObjectSP;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObjectSP;
+    }
+
+    public void ClearKitChenObject()
+    {
+        kitchenObjectSP = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObjectSP != null;
     }
 }
