fc287ac691040eb74ff05ea5f591aace6ec41aaa
manuel taveras <32987336+kamekazz@users.noreply.github.com>
2023-11-08 00:31:09 -0500

    Player Pick up ,Brop objects

--- loca cocina/Assets/Code/Iteam/ClearCounter.cs
+++ loca cocina/Assets/Code/Iteam/ClearCounter.cs
@@ -13,7 +13,24 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player playerSP)
     {
+        if (!HasKitchenObject())
+        {
+            if (playerSP.HasKitchenObject())
+            {
+                playerSP.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else
+        {
+            if (playerSP.HasKitchenObject())
+            {
 
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(playerSP);
+            }
+        }
     }
 
 
